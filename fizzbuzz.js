0f25b697be3a81a3e941cedb6c437ee99b970fba
Albert Ngagom Piewe <ngagompiewe@gmail.com>
2021-10-21 13:31:35 -0400

    master

---
+++ fizzbuzz.js
@@ -0,0 +1,19 @@
+
+
+function fizzbuzz(){
+    var name = prompt('Enter your name', 'Albert');
+    alert(name);
+
+    for(let i =1; i < 100; i ++){
+        if (i % 3 ==0){
+            console.log("Fizz");
+        }if(i % 5 == 0){
+            console.log("Buzz");
+        }if (i % 3 ==0 && i % 5 ==0){
+            console.log("Fizzbuzz");
+        }else{
+            console.log(i);
+        }
+    
+    }
+}
